d5165270caa189a1300ed3d6d1831870676f32e7
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-07 10:48:09 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
-    // Área de la circunferencia = PI * radio˄2
+    // 6. Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar
+    // el resultado en metros/segundo. Velocidad = espacio/tiempo.
 
+    let Kilometro = Number(prompt(`Ingrese una distancia en Km`))
 
-    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
+    let Minutos = Number(prompt(`Ìngrse el tiempo en min`))
 
+    let conversionKmMt = Kilometro * 1000
 
-    console.group(`%cFormula ${2} * ${Math.PI} * ${radio}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    let conversionMinSeg = Minutos * 60
 
-        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
-    console.groupEnd();
+    let velocidad = conversionKmMt / conversionMinSeg
 
-    console.group(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
-    console.groupEnd();
+    console.log(`La velocidad que recorre el proyectil es ${velocidad} mt/seg`); 
+ 
 })
