85aed7faacb6c3d86e6ed156950801bf66d601d5
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-07 11:19:31 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 8. Escribir un programa que evalúe la siguiente expresión:
-    // (a+7*c)/(b+2-a)+2*b 
+    
+    // 9. Escribir un programa que calcule el área y el volumen de un cilindro:
+    // A = (2 * (PI * r˄2)) + ((2 * PI * r) * h)
+    // V = (PI * r2) * h
 
-    let a = Number(prompt(`Ingrese el valor de a`))
-    let b = Number(prompt(`Ingrese el valor de b`))
-    let c = Number(prompt(`Ingrese el valor de c`))
 
-    let rta = (a+7*c)/(b+2-a)+2*b 
+    let r = Number(prompt(`Ingrese el valor del radio del cilindro`))
+    let h = Number(prompt(`Ingrese la altura del cilindro`))
 
-    console.log(`%cLa respuesta es ${rta}`, `background-color: #2aca88; border-radius: 2px; border: 4px groove greenyellow;`)
 
+    let area = (2 * (Math.PI * Math.pow(r,2) ))
+
+    let vol = (Math.PI * Math.pow(r,2) * h)
+
+    console.log(`%cEl area del cilindro es ${area}`, `background-color: #2aca88; border-radius: 2px; border: 4px groove greenyellow;`)
+
+    console.log(`%cEl volumen del cilindro es ${vol}`, `background-color: #f49efc; border-radius: 2px; border: 4px dotted rgb(106, 106, 255);color: black;`)
 })
