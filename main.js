94e51eea7d89b26287ba6b0f57b8dc889ded05e0
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-14 10:13:39 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
     
-   // 10. Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 +
-    // b * x + c, siendo X un valor constante.
+    // 11. Escribir un programa que calcule el volumen de un elipsoide
+    // V = (4/3) * PI * a * b *c
+    
 
     let a = Number(prompt(`Ingrese el valor de a`))
     let b = Number(prompt(`Ingrese el valor de b`))
     let c = Number(prompt(`Ingrese el valor de c`))
 
 
-    let raiz1 = ((-b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a))
+    let elip = ((4/3) * Math.PI * a * b * c)
+
+    console.log(`%cEl volumen del elipsoide es:  ${elip}`, `background-color: #2aca88; border-radius: 2px; border: 4px groove greenyellow;`)
     
-    let raiz2 = ((-b - Math.sqrt(b**2 - 4 * a * c)) / (2 * a))
-
-    console.log(`%cLa raiz 1 es:  ${raiz1}`, `background-color: #2aca88; border-radius: 2px; border: 4px groove greenyellow;`)
-
-    console.log(`%cLa raiz 2 es: ${raiz2}`, `background-color: #f49efc; border-radius: 2px; border: 4px dotted rgb(106, 106, 255);color: black;`)
 })
