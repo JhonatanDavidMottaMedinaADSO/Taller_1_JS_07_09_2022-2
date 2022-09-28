b415b6ca94316a0ed3420ec67d5dde2115ab6ece
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-28 10:50:51 -0500

    Ejercicio 21

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
     
-    // 20. Un vendedor recibe un sueldo base más el 10% de comisión sobre sus ventas. Si en un mes
-    // cualquiera hace tres ventas por valores: v1, v2 y v3, ¿cuánto recibirá por comisión? y ¿cuánto en total sueldo del vendedor?  
+    // 1. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello d = rc((x2-x1)^2 + (y2-y1)^2)
     
-    let sueldo = Number(prompt(`Ingrese su sueldo`))
-    let v1 =  Number(prompt(`Ingrese el valor de la venta 1:`))
-    let v2 =  Number(prompt(`Ingrese el valor de la venta 2::`))
-    let v3 =  Number(prompt(`Ingrese el valor de la venta 3::`))
+    let x1 =  Number(prompt(`Ingrese el valor de x1:`))
+    let x2 =  Number(prompt(`Ingrese el valor de x2:`))
+    let y1 =  Number(prompt(`Ingrese el valor de y1:`))
+    let y2 =  Number(prompt(`Ingrese el valor de y2:`))
+
    
-    let comiVentas = (v1 + v2 +v3) * 0.1
-    let sueldoTotal = (comiVentas + sueldo)
+    let d = Math.sqrt(Math.pow((x2)-(x1),2) + Math.pow((y2)-(y1),2))
 
 
-
-    console.log(`%cUsted recibirá por comisiones ${comiVentas} y el total de su sueldo mas comisiones es ${sueldoTotal}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
+    console.log(`%cLa distancia entre dos puntos es ${d}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
 })
